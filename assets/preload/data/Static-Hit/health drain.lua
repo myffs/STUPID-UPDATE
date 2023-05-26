f67560a9b3091bf97f5cb8e17ce5e25904996867
LilDrippyMyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-05-25 22:58:07 -0400

    few updates and fixes ups

--- assets/preload/data/Static-Hit/health drain.lua
+++
@@ -1,6 +0,0 @@
-function opponentNoteHit()
-    health = getProperty('health')
-    if getProperty('health') > 0.70 then
-        setProperty('health', health- 0.05);
-    end
-end
--- assets/preload/data/Warning/health drain.lua
+++ assets/preload/data/Warning/health drain.lua
@@ -1,6 +1,6 @@
 function opponentNoteHit()
     health = getProperty('health')
-    if getProperty('health') > 0.16 then
-        setProperty('health', health- 0.01);
+    if getProperty('health') > 0.63 then
+        setProperty('health', health- 0.021);
     end
 end
