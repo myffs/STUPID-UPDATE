2a444d9b45b8d73100732f63952fec7557a9e34d
MemeHovy <124418090+MemeHovy@users.noreply.github.com>
2023-08-10 22:52:27 -0400

    compile fixes

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -18,7 +18,6 @@ import openfl.display.BitmapData;
 import sys.FileSystem;
 import sys.io.File;
 #end
-import options.GraphicsSettingsSubState;
 //import flixel.graphics.FlxGraphic;
 import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.graphics.frames.FlxFrame;
